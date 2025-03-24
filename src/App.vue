69fa50f3ca02869fd1263fa25a47080e76b21ae2
ab3642 <ab3642@gmail.com>
2025-03-24 11:57:19 -0400

    navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <script setup>
 import { RouterView } from "vue-router";
+import Navbar from "./components/Navbar.vue";
 </script>
 
 <template>
   <div class="content">
+    <Navbar />
     <RouterView />
   </div>
 </template>
 
 <style>
-.content{
+.content {
   margin: 0 auto;
   max-width: 1200px;
-  padding: 0 20px
+  padding: 0 20px;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <div class="navbar">
+    <nav>
+      <h1><RouterLink :to="{ name: 'home' }">Muso Ninjas</RouterLink></h1>
+      <div class="links">
+        <button>Logout</button>
+        <RouterLink class="btn" :to="{ name: 'signup' }">Signup</RouterLink>
+        <RouterLink class="btn" :to="{ name: 'login' }">Login</RouterLink>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped>
+.navbar {
+  padding: 16px 10px;
+  margin-bottom: 60px;
+  background: white;
+}
+
+nav {
+  display: flex;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+
+  h1 {
+    margin-left: 20px;
+  }
+
+  .links {
+    margin-left: auto;
+
+    a,
+    button {
+      margin-left: 16px;
+      font-size: 14px;
+    }
+  }
+}
+</style>
